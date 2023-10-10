c18103f748ddf392cf06cbeb319c40e2f2e979aa
Rohith-A <35097579+Rohith-A@users.noreply.github.com>
2023-10-10 21:12:49 +0100

    Update queue.js

--- queue.js
+++ queue.js
@@ -1,1 +1,45 @@
+const queue = () => {
+    const q = [];
+    const headIndex = 0;
+    let tailIndex = 0;
 
+    return {
+        enqueue(item) {
+            tailIndex++;
+            return q.push(item)
+        },
+        dequeue() {
+            if(q.length>0) {
+                return q.shift();
+            }
+        },
+        peek() {
+            return q[headIndex];
+        },
+        last() {
+            return q[tailIndex-1];
+        },
+        
+        length() {
+            return q.length;
+        },
+        isEmpty(){
+            return q.length===0;
+        },
+        clear(){
+            q.length = 0;
+        }
+    }
+};
+
+// usage
+useQueue = queue();
+useQueue.enqueue('a');
+useQueue.enqueue('b');
+useQueue.enqueue('c');
+useQueue.enqueue(2);
+//get length
+console.log(useQueue.length());
+
+console.log(useQueue.peek());
+console.log(useQueue.last());
